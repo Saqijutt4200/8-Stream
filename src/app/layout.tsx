36d81d61e66496a82cd3e72e350ecbfcf55d71a8
Hd Movies <109313485+Saqijutt4200@users.noreply.github.com>
2024-12-24 02:41:50 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "8 Stream",
+  title: "vidscr",
   generator: "Next.js",
   description: "Watch Movies and TV Shows Online For Free",
   manifest: "/manifest.json",
