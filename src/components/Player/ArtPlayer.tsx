69553b93fc183d41e4cb0cc8ca749c293ff440b7
officialbube <ebubenkereebube@gmail.com>
2025-02-01 02:13:59 +0100

    1/2/25

--- src/components/Player/ArtPlayer.tsx
+++ src/components/Player/ArtPlayer.tsx
@@ -469,7 +469,7 @@ export default function Player({
         },
       ],
       plugins: [],
-      customType: setupHLSCustomType(art),
+      customType: setupHLSCustomType(),
     });
 
     // Event listeners
