22f256a27fb8ce88256bdb588253675b49734749
Hd Movies <109313485+Saqijutt4200@users.noreply.github.com>
2025-02-12 02:35:10 +0000

    Update ArtPlayer.tsx

--- src/components/Player/ArtPlayer.tsx
+++ src/components/Player/ArtPlayer.tsx
@@ -121,9 +121,7 @@ export default function Player({
     art.controls.add({
       name: "backward",
       position: "left",
-      html: `<svg width="20" height="20" viewBox="0 0 24 24">
-        <path fill="white" d="M13 18v-2H7v2H5v-2H3V8h2V6h2v2h6V6h2v2h2v8h-2v2h-2Zm-2-4h8V10h-8Zm-6 0h4V10H5Z"/>
-      </svg>`,
+      html: `<svg width="113px" height="113px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.4" d="M9.53906 15.92V10.5801L8.03906 12.2501" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M10.0195 4.46997L11.9995 2" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M4.90939 7.79974C3.79939 9.27974 3.10938 11.1097 3.10938 13.1097C3.10938 18.0197 7.08939 21.9998 11.9994 21.9998C16.9094 21.9998 20.8894 18.0197 20.8894 13.1097C20.8894 8.19974 16.9094 4.21973 11.9994 4.21973C11.3194 4.21973 10.6594 4.30978 10.0194 4.45978" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path opacity="0.4" d="M14 10.5801C15.1 10.5801 16 11.4801 16 12.5801V13.9301C16 15.0301 15.1 15.9301 14 15.9301C12.9 15.9301 12 15.0301 12 13.9301V12.5801C12 11.4701 12.9 10.5801 14 10.5801Z" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`,
       click: () => {
         art.currentTime = Math.max(0, art.currentTime - 10);
       },
